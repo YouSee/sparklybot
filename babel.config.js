087d11618739a67256ef9148bc70906ab05c99b5
René Gustafson <renegustafson@gmail.com>
2019-07-17 12:36:33 +0200

    ts and stuff

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = api => {
           },
         },
       ],
+      '@babel/typescript'
     ],
   }
 }
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+interface TestOptions {
+  sparkApplicationPath: string,
+  port?: number,
+  wsPort?: number,
+  host?: string,
+}
+
+const initializeTest = (testOptions: TestOptions) => {
+  
+}
