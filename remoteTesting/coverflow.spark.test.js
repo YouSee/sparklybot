4b2ca193ba5356ad74dbda6b0ffac171aa633c7e
René Gustafson <renegustafson@gmail.com>
2019-08-02 10:45:27 +0200

    testing without delay

--- remoteTesting/coverflow.spark.test.js
+++ remoteTesting/coverflow.spark.test.js
@@ -50,3 +50,31 @@ test('Can test remote websites', async done => {
   expect(element2).toBeTruthy()
   done()
 })
+
+test('Can test without delay', async done => {
+  await refreshSparkBrowser(
+    'https://www.sparkui.org/examples/coverflow/coverflow_launch.js',
+  )
+  const element = await findElementWithPropertyValue(
+    'text',
+    'Everything Is Copy - Nora Ephron: Scripted & Unscripted',
+  )
+  expect(element).toBeTruthy()
+  // Navigate right
+  await sendKeyEvent('onKeyDown', '39')
+  await sendKeyEvent('onKeyDown', '39')
+  await sendKeyEvent('onKeyDown', '39')
+  await sendKeyEvent('onKeyDown', '39')
+  await sendKeyEvent('onKeyDown', '37')
+  await sendKeyEvent('onKeyDown', '37')
+  await sendKeyEvent('onKeyDown', '13')
+  // Assert element
+  const element1 = findElementWithPropertyValue('text', 'Christine')
+  expect(element1).toBeTruthy()
+  await sendKeyEvent('onKeyDown', '39')
+  await sendKeyEvent('onKeyDown', '13')
+  // Assert element
+  const element2 = findElementWithPropertyValue('text', 'Deadpool')
+  expect(element2).toBeTruthy()
+  done()
+})
